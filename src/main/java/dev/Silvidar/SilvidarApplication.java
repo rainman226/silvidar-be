05d47145de843e58e9b1196a07648745d104440e
rainman226 <dota2romaniarobi@gmail.com>
2025-01-14 21:11:50 +0200

    added initial project files

---
+++ src/main/java/dev/Silvidar/SilvidarApplication.java
@@ -0,0 +1,13 @@
+package dev.Silvidar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilvidarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilvidarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Silvidar/SilvidarApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Silvidar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvidarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
